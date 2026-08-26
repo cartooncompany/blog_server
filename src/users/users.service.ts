207197e9025a48a00d3f9d07803a7317e159765e
taejun <ptj01036251@gmail.com>
2026-08-26 11:08:03 +0900

    feat: 기본 users 폴더 생성 후 회원가입 기능 개발 (#3)

---
+++ src/users/users.service.ts
@@ -0,0 +1,47 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password, name } = createUserDto;
+
+    //이메일 중복
+    const existing = await this.prisma.user.findUnique({where : { email }})
+    if (existing) {
+      throw new ConflictException("이미 사용중인 이메일입니다.");
+    }
+
+    //비밀번호 해싱
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //DB 저장
+    const user = await this.prisma.user.create({
+      data: {email, password: hashedPassword, name},
+    });
+
+    const {password: _, ...result} = user;
+    return result;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
