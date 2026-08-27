a3ed369060e4c6acac2713b266477fe68f0bd59e
taejun <ptj01036251@gmail.com>
2026-08-27 09:49:03 +0900

    feat: auth 기본 뼈대 생성 (#5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
