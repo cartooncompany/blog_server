7cdfb340ba66964d640383848865552004e9a662
taejun <ptj01036251@gmail.com>
2026-08-31 10:00:36 +0900

    feat: Posts CRUD 개발 (#7)

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { ConflictException, ForbiddenException, Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto.js';
+import { UpdatePostDto } from './dto/update-post.dto.js';
+import { PrismaService } from '../../prisma/prisma.service.js';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(private readonly prisma: PrismaService) {}
+
+  //게시글 생성
+  async create(authorId: number, dto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: {
+        title: dto.title,
+        content: dto.content,
+        authorId,
+      }
+    });
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  //게시글 전체 조회
+  async findAll() {
+    return this.prisma.post.findMany({
+      include: {
+        author: { select: { id: true, name: true } },
+      },
+      orderBy: { createdAt: 'desc' },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  //게시글 상세 조회
+  async findOne(id: number) {
+    const post = await this.prisma.post.findUnique({
+      where: { id },
+      include: {
+        author: { select: { id: true, name: true } },
+      },
+    });
+    return post;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, userId: number, dto: UpdatePostDto) {
+    const post = await this.findOne(id);
+    if(post?.authorId !== userId) {
+      throw new ForbiddenException('수정 권환이 없습니다');
+    }
+    return this.prisma.post.update({
+      where: { id },
+      data: dto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number, userId: number) {
+    const post = await this.findOne(id);
+    if(post?.authorId !== userId) {
+      throw new ForbiddenException('삭제 권환이 없습니다.');
+    }
+    await this.prisma.post.delete({ where: { id } });
+    return { message: '삭제되었습니다' };
   }
 }
