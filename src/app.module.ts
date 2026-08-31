43c1a48a7a96c5659b99ec3e1bb58a4957c88b08
taejun <ptj01036251@gmail.com>
2026-08-31 09:07:46 +0900

    feat: posts CRUD 뼈대 생성 (#7)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
 import { AuthModule } from './auth/auth.module.js';
+import { PostsModule } from './posts/posts.module';
 
 
 @Module({
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module.js';
     PrismaModule,
     UsersModule,
     AuthModule,
+    PostsModule,
   ],
 })
 export class AppModule {}
